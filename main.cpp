6cfd2fe4fc3c398a24ed324e9090d1c6f2a970f3
Jay Montoya <jayjoplin9898@gmail.com>
2019-03-14 20:01:12 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/*
+ * CS 151
+ * Group Project
+ * Programmers:
+ *      Jay Montoya
+ *      Nathanael Scudder
+ *      Monika Shin
+ *      Christa Hatch
+ * 
+ *
+ */
+#include <iostream>
+using namespace std;
+int main() {
+
+    string months[] = {"January", "February", "March", "April", "May", "June", "July", "August", "Setpember",
+                       "October", "November", "December"};
+    int daysInMonth[] = {31, 28,31,30,31,30,31,31,30,31,30,31};
+
+
+    return 0;
+}
